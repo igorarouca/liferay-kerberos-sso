d6b10488e4ff3fa038155c4b1aeb871fd9e7cbf6
Igor Arouca <igor.arouca@liferay.com>
2015-08-06 16:31:14 -0700

    Rename plugin site-cleanup-portlet to db-cleanup-portlet

---
+++ portlets/db-cleanup-portlet/docroot/WEB-INF/src/com/cambiahealth/portal/dbcleanup/cleaners/site/impl/AbstractSiteCleaner.java
@@ -0,0 +1,258 @@
+package com.cambiahealth.portal.dbcleanup.cleaners.site.impl;
+
+import com.cambiahealth.portal.dbcleanup.DbCleanupConstants;
+import com.cambiahealth.portal.dbcleanup.cleaners.CorruptedDataCleanerUtil;
+import com.cambiahealth.portal.dbcleanup.cleaners.site.SiteCleaner;
+import com.cambiahealth.portal.dbcleanup.util.StagingAdvicesUtil;
+
+import com.liferay.portal.kernel.dao.orm.ORMException;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistryUtil;
+import com.liferay.portal.kernel.search.SearchEngineUtil;
+import com.liferay.portal.kernel.search.SearchException;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.Layout;
+import com.liferay.portal.service.GroupLocalServiceUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static com.cambiahealth.portal.dbcleanup.DbCleanupConstants.BULK_REINDEX_ENABLED;
+public abstract class AbstractSiteCleaner implements SiteCleaner {
+
+	@Override
+	public List<Group> call() {
+		final Thread currentThread = Thread.currentThread();
+		final String oldThreadName = currentThread.getName();
+		currentThread.setName("SiteCleaner-Call");
+
+		List<Group> removedSites = Collections.emptyList();
+
+		if (!hasSitesToRemove()) {
+			return removedSites;
+		}
+
+		long startTime = System.currentTimeMillis();
+
+		boolean indexReadOnly = SearchEngineUtil.isIndexReadOnly();
+		_log.info(">>> Search index read only: " + indexReadOnly);
+
+		if (BULK_REINDEX_ENABLED) {
+			SearchEngineUtil.setIndexReadOnly(true);
+			_log.info(">>> Disabled search indexing");
+		}
+
+		try {
+			removedSites = doCall();
+		}
+		finally {
+			currentThread.setName(oldThreadName);
+
+			if (BULK_REINDEX_ENABLED) {
+				SearchEngineUtil.setIndexReadOnly(indexReadOnly);
+				_log.info(">>> Restored indexReadOnly value");
+
+				triggerBulkReindex();
+			}
+
+			shutdown();
+		}
+
+		long endTime = System.currentTimeMillis();
+		long duration = TimeUnit.MILLISECONDS.toMinutes(endTime - startTime);
+
+		if (_log.isInfoEnabled()) {
+			_log.info(">>> Removed " + removedSites.size() + " sites: " +
+				asString(removedSites) + " in " + duration + " min");
+
+			if (hasSitesToRemove()) {
+				_log.info("There is (are) " + _sitesToBeRemoved.size() +
+					" site(s) that could not be removed: " +
+						asString(_sitesToBeRemoved));
+			}
+		}
+
+		cleanOrphanData(removedSites);
+
+		return removedSites;
+	}
+
+	@Override
+	public boolean hasSitesToRemove() {
+		return !_sitesToBeRemoved.isEmpty();
+	}
+
+	protected AbstractSiteCleaner(long companyId, List<String> siteNames) {
+		if ((siteNames == null) || siteNames.isEmpty()) {
+			throw new IllegalArgumentException("Site list is empty!");
+		}
+
+		_companyId = companyId;
+		_sitesToBeRemoved = new ArrayList<>();
+
+		Collections.sort(siteNames);
+		for (String name : siteNames) {
+			Group site = null;
+			try {
+				site = getSiteBy(_companyId, name);
+			}
+			catch (ORMException orme) {
+				_log.warn(
+					">>> Failed to retrieve site: " + name + ". Retrying...",
+					orme);
+
+				site = getSiteBy(_companyId, name);
+			}
+
+			if (site == null) {
+				_log.warn(">>> Site not found: " + name);
+			}
+			else {
+				_log.info(">>> Loaded site: " + name);
+				_sitesToBeRemoved.add(site);
+			}
+		}
+
+		if (hasSitesToRemove()) {
+			_log.info(">>> Loaded " + _sitesToBeRemoved.size() +
+				" site(s) to be removed.");
+		}
+	}
+
+	protected String asString(Group group) {
+		return "{groupId=" + group.getGroupId() +
+			", name=" + group.getName() + "}";
+	}
+
+	protected String asString(Layout layout) {
+		return "{plid=" + layout.getPlid() + ", friendlyURL=" +
+				layout.getFriendlyURL() + "}";
+	}
+
+	protected String asString(List<Group> sites) {
+		StringBuilder siteList = new StringBuilder();
+
+		for (Group site : sites) {
+			siteList.append(site.getName());
+			siteList.append(StringPool.COMMA).append(StringPool.SPACE);
+		}
+
+		int length = siteList.length();
+		if (length > 0) {
+			siteList.setLength(length - 2);
+		}
+
+		return siteList.toString();
+	}
+
+	protected void cleanOrphanData(List<Group> removedSites) {
+		String groupIdCommaSeparatedList =
+			DbCleanupConstants.DANGLING_GROUP_IDS;
+
+		if (groupIdCommaSeparatedList.isEmpty()) {
+			_log.warn(">>> Property 'db.cleanup.dangling.group.ids' is empty");
+
+			if (removedSites.isEmpty()) {
+				return;
+			}
+		}
+
+		long[] danglingGroupIds = StringUtil.split(
+			groupIdCommaSeparatedList, 0l);
+
+		_log.info(
+			">>> 'db.cleanup.dangling.group.ids' = " +
+				Arrays.toString(danglingGroupIds));
+
+		// Combine dangling groupIds with groupIds from removed sites
+		int middleIndex = danglingGroupIds.length;
+		long[] combinedGroupIds = new long[middleIndex + removedSites.size()];
+
+		for (int i = 0; i < middleIndex; ++i) {
+			combinedGroupIds[i] = danglingGroupIds[i];
+		}
+
+		int j = middleIndex;
+		for (Group removedSite : removedSites) {
+			combinedGroupIds[j] = removedSite.getGroupId();
+			++j;
+		}
+
+		CorruptedDataCleanerUtil.clean(combinedGroupIds);
+	}
+
+	protected abstract List<Group> doCall();
+
+	protected Group getSiteBy(long companyId, String name) {
+
+		Group site = null;
+		try {
+			site = GroupLocalServiceUtil.fetchGroup(companyId, name);
+		}
+		catch (SystemException se) {
+			_log.error(">>> Error looking up site: " + name, se);
+		}
+
+		return site;
+	}
+
+	protected void reindex(Indexer indexer) {
+		try {
+			indexer.reindex(new String[] { String.valueOf(_companyId) });
+
+			_log.info(">>> Finished reindex for: " +
+				indexer.getClass().getSimpleName());
+		}
+		catch (SearchException se) {
+			_log.error(">>> Error executing bulk re-index", se);
+		}
+	}
+
+	protected Group remove(final Group site) {
+		Group removedSite = null;
+
+		boolean stagingAdvicesEnabled =
+			StagingAdvicesUtil.disableStagingAdvices();
+
+		try {
+			removedSite = GroupLocalServiceUtil.deleteGroup(site);
+
+			if (_log.isInfoEnabled()) {
+				_log.info(">>> Removed site: " + asString(site));
+			}
+
+		}
+		catch (PortalException | SystemException e) {
+			_log.error(">>> Error removing site: " + asString(site), e);
+		}
+		finally {
+			StagingAdvicesUtil.resetStagingAdvices(stagingAdvicesEnabled);
+		}
+
+		return removedSite;
+	}
+
+	protected abstract void shutdown();
+
+	protected void triggerBulkReindex() {
+		for (Indexer indexer : IndexerRegistryUtil.getIndexers()) {
+			reindex(indexer);
+		}
+	}
+
+	protected final long _companyId;
+
+	protected final List<Group> _sitesToBeRemoved;
+
+	private static Log _log = LogFactoryUtil.getLog(AbstractSiteCleaner.class);
+
+}
---
+++ portlets/db-cleanup-portlet/docroot/WEB-INF/src/com/cambiahealth/portal/dbcleanup/cleaners/site/impl/ParallelSiteCleaner.java
@@ -0,0 +1,169 @@
+package com.cambiahealth.portal.dbcleanup.cleaners.site.impl;
+
+import com.cambiahealth.portal.dbcleanup.cleaners.site.SiteCleaner;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.model.Group;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import static com.cambiahealth.portal.dbcleanup.DbCleanupConstants.SITE_REMOVAL_TIMEOUT;
+import static com.cambiahealth.portal.dbcleanup.DbCleanupConstants.THREAD_POOL_AUTOSIZING_ENABLED;
+import static com.cambiahealth.portal.dbcleanup.DbCleanupConstants.THREAD_POOL_SIZE;
+class ParallelSiteCleaner extends AbstractSiteCleaner implements SiteCleaner {
+
+	@Override
+	public boolean isRunning() {
+		return _removeTasks != null && !_removeTasks.isEmpty();
+	}
+
+	@Override
+	public boolean isTerminated() {
+		return _threadExecutor.isTerminated();
+	}
+
+	protected List<Group> collectRemoveTasksResults() {
+		List<Group> removedSites = new ArrayList<>();
+
+		Iterator<Future<Group>> removeTasksIterator = _removeTasks.iterator();
+
+		while (removeTasksIterator.hasNext()) {
+			Future<Group> task = removeTasksIterator.next();
+
+			Group site = null;
+			try {
+				site = task.get();
+			}
+			catch (ExecutionException ignored) {
+				// Already logged during execution
+			}
+			catch (InterruptedException ie) {
+				_log.error(ie, ie);
+			}
+			finally {
+				removeTasksIterator.remove();
+			}
+
+			if (site != null) {
+				_sitesToBeRemoved.remove(site);
+				removedSites.add(site);
+			}
+		}
+
+		return removedSites;
+	}
+
+	@Override
+	protected List<Group> doCall() {
+		_log.info(">>> Site removal set to run in parallel");
+
+		_removeTasks = new ArrayList<Future<Group>>();
+
+		CountDownLatch siteRemovalCounter =
+			new CountDownLatch(_sitesToBeRemoved.size());
+
+		for (Group site : _sitesToBeRemoved) {
+			_removeTasks.add(_threadExecutor.submit(
+				newRemoveTaskFor(site, siteRemovalCounter)));
+		}
+
+		try {
+			_log.info(">>> Site Removal timeout set to: " +
+				SITE_REMOVAL_TIMEOUT + " min");
+
+			siteRemovalCounter.await(SITE_REMOVAL_TIMEOUT, TimeUnit.MINUTES);
+		}
+		catch (InterruptedException ie) {
+			_log.error(ie, ie);
+		}
+
+		return collectRemoveTasksResults();
+	}
+
+	protected Runnable newReindexTaskFor(final Indexer indexer) {
+		return new Runnable() {
+
+			@Override
+			public void run() {
+				final Thread currentThread = Thread.currentThread();
+				final String oldThreadName = currentThread.getName();
+				currentThread.setName(
+					"Reindexing-" + indexer.getClass().getSimpleName());
+
+				try {
+					ParallelSiteCleaner.super.reindex(indexer);
+				}
+				finally {
+					currentThread.setName(oldThreadName);
+				}
+			}
+		};
+	}
+
+	protected Callable<Group> newRemoveTaskFor(
+		final Group site, final CountDownLatch siteRemovalCounter) {
+
+		return new Callable<Group>() {
+			@Override
+			public Group call() {
+				final Thread currentThread = Thread.currentThread();
+				final String oldThreadName = currentThread.getName();
+				currentThread.setName("SiteCleaner-" + site.getGroupId());
+
+				try {
+					return remove(site);
+				}
+				finally {
+					currentThread.setName(oldThreadName);
+					siteRemovalCounter.countDown();
+				}
+			}
+		};
+	}
+
+	@Override
+	protected void reindex(Indexer indexer) {
+		_threadExecutor.submit(newReindexTaskFor(indexer));
+	}
+
+	@Override
+	protected void shutdown() {
+		_threadExecutor.shutdownNow();
+		_log.info(">>> Shutdown thread executor");
+	}
+
+	ParallelSiteCleaner(long companyId, List<String> siteNames) {
+		super(companyId, siteNames);
+
+		if (THREAD_POOL_AUTOSIZING_ENABLED) {
+			_threadExecutor = Executors.newCachedThreadPool();
+
+			_log.info(">>> Parallel removal set to use a cached thread pool");
+		}
+		else {
+			_threadExecutor = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+
+			_log.info(
+				">>> Parallel removal set to use a fixed thread pool with " +
+					THREAD_POOL_SIZE + " threads");
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ParallelSiteCleaner.class);
+
+	private List<Future<Group>> _removeTasks;
+	private final ExecutorService _threadExecutor;
+
+}
