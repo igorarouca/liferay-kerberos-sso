37d4212cf0b16981b05c1edccc77f2d9a06429d5
Igor Arouca <igor.arouca@liferay.com>
2015-09-25 12:17:14 -0700

    Add code to migrate P13N portlet preferences

---
+++ portlets/db-cleanup-portlet/docroot/WEB-INF/src/com/cambiahealth/portal/dbcleanup/cleaner/p13n/portletpreferences/PortletPreferencesMigrationUtil.java
@@ -0,0 +1,75 @@
+package com.cambiahealth.portal.dbcleanup.cleaner.p13n.portletpreferences;
+
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.dao.orm.Property;
+import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.model.PortletPreferences;
+import com.liferay.portal.service.PortletPreferencesLocalServiceUtil;
+
+import java.util.List;
+public class PortletPreferencesMigrationUtil {
+
+	@SuppressWarnings("unchecked")
+	public static void migratePortletPreferences(long companyId) {
+		DynamicQuery query = PortletPreferencesLocalServiceUtil.dynamicQuery();
+
+		query.add(_PORTLET_ID_QUERY_PROPERTY.like(_P13N_PORTLET_ID));
+		query.add(
+			_PREFERENCES_QUERY_PROPERTY.like(_ARTICLE_ENTRIES_PREFERENCES_KEY));
+
+		List<PortletPreferences> portletPreferencesList = null;
+		try {
+			portletPreferencesList =
+				PortletPreferencesLocalServiceUtil.dynamicQuery(query);
+
+		} catch (SystemException se) {
+			_log.error(">>> Error retrieving for P13N portlet preferences", se);
+		}
+
+		if ((portletPreferencesList == null)
+			|| portletPreferencesList.isEmpty()) {
+
+			return;
+		}
+
+		for (PortletPreferences portletPreferences : portletPreferencesList) {
+			migrate(companyId, portletPreferences);
+		}
+	}
+
+	protected static void migrate(
+		long companyId, PortletPreferences portletPreferences) {
+
+		PortletPreferencesMigrationImpl migration =
+			new PortletPreferencesMigrationImpl(companyId, portletPreferences);
+
+		try {
+			migration.run();
+		}
+		catch (Exception e) {
+			_log.error(e);
+		}
+	}
+
+	private PortletPreferencesMigrationUtil() {
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		PortletPreferencesMigrationUtil.class);
+
+	private static final String _ARTICLE_ENTRIES_PREFERENCES_KEY =
+		"%articleEntries%";
+
+	private static final String _P13N_PORTLET_ID =
+		"contentwebdisplay_WAR_contentwebdisplayportlet_%";
+
+	private static final Property _PORTLET_ID_QUERY_PROPERTY =
+		PropertyFactoryUtil.forName("portletId");
+
+	private static final Property _PREFERENCES_QUERY_PROPERTY =
+		PropertyFactoryUtil.forName("preferences");
+
+}
