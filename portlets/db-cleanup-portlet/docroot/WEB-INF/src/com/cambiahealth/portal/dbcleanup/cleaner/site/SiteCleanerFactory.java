f9da76d64fafea134861c11933a85d690a2a809e
Igor Arouca <igor.arouca@liferay.com>
2015-11-19 11:52:24 -0500

    Remove db-cleanup-portlet from journal-uuid branch

--- portlets/db-cleanup-portlet/docroot/WEB-INF/src/com/cambiahealth/portal/dbcleanup/cleaner/site/SiteCleanerFactory.java
+++
@@ -1,8 +0,0 @@
-package com.cambiahealth.portal.dbcleanup.cleaner.site;
-
-import java.util.List;
-public interface SiteCleanerFactory {
-
-	SiteCleaner newSiteCleaner(long companyId, List<String> siteNames);
-
-}
