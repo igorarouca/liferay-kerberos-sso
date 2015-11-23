bac452e561b971a1ce24b4ca8df5341f44a52f95
Igor Arouca <igor.arouca@liferay.com>
2015-11-23 15:16:54 -0500

    Add implementation for KerberosAutoLogin

---
+++ hooks/kerberos-sso-hook/docroot/WEB-INF/src/com/liferay/portal/security/auth/KerberosAutoLogin.java
@@ -0,0 +1,108 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.portal.security.auth;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.model.Company;
+import com.liferay.portal.model.User;
+import com.liferay.portal.security.auth.util.KerberosAuthSettingsUtil;
+import com.liferay.portal.security.ldap.PortalLDAPImporterUtil;
+import com.liferay.portal.service.UserLocalServiceUtil;
+import com.liferay.portal.util.PortalUtil;
+public class KerberosAutoLogin implements AutoLogin {
+
+	@Override
+	public String[] login(
+			HttpServletRequest request, HttpServletResponse response)
+		throws AutoLoginException {
+
+		try {
+			if (!KerberosAuthSettingsUtil.isAuthEnabled()) {
+				_log.debug(">>> Kerberos authentication is disabled");
+
+				return null;
+			}
+
+			String userPrincipalName = request.getRemoteUser();
+
+			if (Validator.isNull(userPrincipalName)) {
+				_log.debug(">>> Kerberos UPN is null");
+
+				return null;
+			}
+
+			int slashIndex = userPrincipalName.lastIndexOf(
+				StringPool.BACK_SLASH);
+			String userName = userPrincipalName.substring(++slashIndex);
+
+			Company company = PortalUtil.getCompany(request);
+			long companyId = company.getCompanyId();
+			User user = null;
+
+			if (KerberosAuthSettingsUtil.isLDAPImportEnabled()) {
+				_log.debug(">>> User import from LDAP is enabled");
+
+				try {
+					user = PortalLDAPImporterUtil.importLDAPUser(
+						companyId, StringPool.BLANK, userName);
+				}
+				catch (Exception se) {
+					_log.error(">>> Error importing user from LDAP", se);
+				}
+			}
+			else {
+				_log.debug(">>> User import from LDAP is disabled");
+			}
+
+			if (user == null) {
+				_log.debug(">>> Fetching user by screenName from database");
+
+				user = UserLocalServiceUtil.fetchUserByScreenName(
+					companyId, userPrincipalName);
+			}
+
+			if (user == null) {
+				return null;
+			}
+
+			String[] credentials = new String[3];
+
+			credentials[0] = String.valueOf(user.getUserId());
+			credentials[1] = user.getPassword();
+			credentials[2] = StringPool.TRUE;
+
+			return credentials;
+		}
+		catch (Exception e) {
+			_log.error(">>> Error executing Kerberos auto login", e);
+
+			return null;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return this.getClass().getSimpleName();
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(KerberosAutoLogin.class);
+
+}
---
+++ hooks/kerberos-sso-hook/docroot/WEB-INF/src/com/liferay/portal/security/auth/util/KerberosPropsValues.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.portal.security.auth.util;
+
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.PropsUtil;
+public class KerberosPropsValues {
+
+	public static final boolean KERBEROS_AUTH_ENABLED =
+		GetterUtil.getBoolean(
+			PropsUtil.get(KerberosPropsKeys.KERBEROS_AUTH_ENABLED));
+
+	public static final boolean KERBEROS_IMPORT_FROM_LDAP =
+		GetterUtil.getBoolean(PropsUtil.get(
+			KerberosPropsKeys.KERBEROS_IMPORT_FROM_LDAP));
+
+	private KerberosPropsValues() {
+	}
+
+}
